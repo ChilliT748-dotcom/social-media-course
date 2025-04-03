1016eba7a9ba63f7c22350d8fb8ccd005c3e4547
ChilliT748-dotcom <taylorcobus@gmail.com>
2025-04-04 10:31:25 +1300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,18 @@
+import Link from 'next/link';
+
+export default function HomePage() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Welcome to the Social Media Course</h1>
+      <p>Select a module to begin:</p>
+      <ul>
+        {[...Array(10)].map((_, i) => (
+          <li key={i}>
+            <Link href={`/modules/${i + 1}`}>Go to Module {i + 1}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 // Home page
